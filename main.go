39cd8197803329ce2104c44a15e359ff4020c5b3
Alan Cuevas <alancuevas@Alans-MacBook-Pro.local>
2022-08-11 23:09:49 -0400

    Variables y constates

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hoola mundo")
+	// Constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	//Variables
+	base := 12 //: para declarar e asignar
+	var altura int = 14
+	var area int
+	area = base * altura
+	fmt.Println(area)
+
+	//Zero Values valores nuevos tienen un valor
+	var a int     //0
+	var b float64 //0
+	var c string  //sting vacio
+	var d bool    //false
+	fmt.Println(a, b, c, d)
+
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
 }
